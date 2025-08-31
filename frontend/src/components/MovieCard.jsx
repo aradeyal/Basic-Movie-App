584f5c195fad8d533f840101f6ec71ee3fcf424f
Arad Eyal <arad.eyal10@gmail.com>
2025-08-31 20:54:53 +0300

    new improvements to the css

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,30 +1,55 @@
-import "../css/MovieCard.css"
-import { useMovieContext } from "../contexts/MovieContext"
+import "../css/MovieCard.css";
+import { useMovieContext } from "../contexts/MovieContext";
 
-function MovieCard({movie}) {
-    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
-    const favorite = isFavorite(movie.id)
+function MovieCard({ movie }) {
+  const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
+  const favorite = isFavorite(movie.id);
 
-    function onFavoriteClick(e) {
-        e.preventDefault()
-        if (favorite) removeFromFavorites(movie.id)
-        else addToFavorites(movie)
-    }
+  function onFavoriteClick(e) {
+    e.preventDefault();
+    if (favorite) removeFromFavorites(movie.id);
+    else addToFavorites(movie);
+  }
 
-    return <div className="movie-card">
-        <div className="movie-poster">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
-            <div className="movie-overlay">
-                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
-                    ♥
-                </button>
-            </div>
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+
+        {/* שכבת overlay עם ריווח כדי שהכפתור לא ייחתך בקצה */}
+        <div className="movie-overlay">
+          <button
+            className={`favorite-btn ${favorite ? "active" : ""}`}
+            onClick={onFavoriteClick}
+            aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+            aria-pressed={favorite}
+            type="button"
+          >
+            {favorite ? (
+              /* bi-bookmark-heart-fill */
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
+                <path d="M2 15.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2zM8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
+              </svg>
+            ) : (
+              /* bi-bookmark-heart */
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
+                <path fillRule="evenodd" d="M8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
+                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
+              </svg>
+            )}
+          </button>
         </div>
-        <div className="movie-info">
-            <h3>{movie.title}</h3>
-            <p>{movie.release_date?.split("-")[0]}</p>
-        </div>
+      </div>
+
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date?.split("-")[0]}</p>
+      </div>
     </div>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,15 +1,44 @@
-import {Link} from "react-router-dom"; 
+import { Link, useLocation } from "react-router-dom";
 import "../css/Navbar.css";
-function NavBar(){
-    return <nav className="navbar">
-        <div className="navbar-brand">
-            <Link to ="/">Movie App</Link>
+
+function NavBar() {
+  const location = useLocation();
+
+  return (
+    <nav className="navbar">
+      <div className="nav-inner">
+        <div className="brand">
+          <Link to="/">Movie<span>App</span></Link>
         </div>
-        <div className="navbar-links">
-            <Link to ="/" className="nav-link">Home</Link>
-            <Link to ="/favorites" className="nav-link">Favorites</Link>
-        </div>
-        </nav>
+
+        <ul className="nav-links">
+          <li>
+            <Link
+              to="/"
+              className={`nav-link ${location.pathname === "/" ? "active" : ""}`}
+            >
+              <svg viewBox="0 0 24 24" aria-hidden="true">
+                <path d="M12 3 2.5 11h2v9h5v-6h5v6h5v-9h2L12 3z" />
+              </svg>
+              <span>Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/favorites"
+              className={`nav-link ${location.pathname === "/favorites" ? "active" : ""}`}
+            >
+              <svg viewBox="0 0 16 16" aria-hidden="true">
+                <path fillRule="evenodd" d="M8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
+                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
+              </svg>
+              <span>Favorites</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
